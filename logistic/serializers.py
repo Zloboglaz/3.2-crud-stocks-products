3cd9821d037e513f2e5d641c257c7b0a1995bb11
Andrey Gridnev <gridnevandr@yandex.ru>
2026-02-19 22:52:03 +0300

    Homework solution

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -1,43 +1,58 @@
 from rest_framework import serializers
 
+from .models import Product, Stock, StockProduct
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    # +настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = "__all__"
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    # +настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'price', 'quantity']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    # +настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
         # создаем склад по его параметрам
-        stock = super().create(validated_data)
+        stock = Stock.objects.create(**validated_data)
 
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
         return stock
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
+        instance.address = validated_data.get('address', instance.address)
+        instance.save()
+        instance.positions.all().delete()
 
-        # обновляем склад по его параметрам
-        stock = super().update(instance, validated_data)
+        # # обновляем склад по его параметрам
+        # stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
-        return stock
+        # # здесь вам надо обновить связанные таблицы
+        # # в нашем случае: таблицу StockProduct
+        # # с помощью списка positions
+        # # return stock
+        for position in positions:
+            StockProduct.objects.create(stock=instance, **position)
+        return instance
--- logistic/views.py
+++ logistic/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -8,9 +9,20 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter]
+
+    def get_queryset(self):
+        queryset = Stock.objects.all()
+        search_query = self.request.query_params.get('search', None)
+        if search_query:
+            # Ищем склады, где есть продукт с именем, содержащим search_query
+            queryset = queryset.filter(positions__product__title__icontains=search_query).distinct()
+        return queryset
